74d1f1df9149ef0cbae7e68c9fd5703351c51ccf
LeeMcQ <mcquir4l@gmail.com>
2026-03-23 07:27:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'logos-living-v7';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'fellowship-faceoff-v10';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './mode-sing.png', './mode-act.png', './mode-explain.png', './mode-champ.png', './mode-library.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
